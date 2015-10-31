0bb3175396a43d0ff50348cdcff8485371bd374f
devill <devillmeister@gmail.com>
2015-10-31 11:35:20 +0000

    Bit less agression

--- player.rb
+++ player.rb
@@ -4,7 +4,12 @@ class Player
   VERSION = "All in strategy"
 
   def bet_request(game_state)
-    10000
+    hole_cards = game_state['players'][game_state['in_action']]['hole_cards']
+    if hole_cards[0]['rank'] == 'A' and hole_cards[1]['rank'] == 'A'
+      10000
+    else
+      1000
+    end
   end
 
   def showdown(game_state)
