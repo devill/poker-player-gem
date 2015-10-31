8e1f3fc814ad8d013eeb10ea9da3d51709255698
devill <devillmeister@gmail.com>
2015-10-31 12:03:23 +0000

    YOLOOO

--- player.rb
+++ player.rb
@@ -5,9 +5,13 @@ class Player
 
   def bet_request(game_state)
     hole_cards = game_state['players'][game_state['in_action']]['hole_cards']
+    number_of_active_players = game_state['players'].count { |player| player['status'] != 'out' }
+
     if high?(hole_cards[0]) and high?(hole_cards[1])
       10000
     elsif high?(hole_cards[0]) or high?(hole_cards[1])
+      200
+    elsif number_of_active_players < 3
       200
     else
       0
