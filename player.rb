a5bb210bc4251da0c0abeb6371f936fdb9c0a31d
devill <devillmeister@gmail.com>
2015-10-31 12:20:56 +0000

    Conservative preflop

--- player.rb
+++ player.rb
@@ -1,18 +1,18 @@
 
 class Player
 
-  VERSION = "High card strategy"
+  VERSION = "Conservative preflop"
 
   def bet_request(game_state)
     hole_cards = game_state['players'][game_state['in_action']]['hole_cards']
     number_of_active_players = game_state['players'].count { |player| player['status'] != 'out' }
 
-    if high?(hole_cards[0]) and high?(hole_cards[1])
+    if hole_cards[0]['rank'] == 'A' and hole_cards[1]['rank'] == 'A'
       10000
-    elsif high?(hole_cards[0]) or high?(hole_cards[1])
-      1000
-    elsif number_of_active_players < 3
-      1000
+    elsif number_of_active_players > 2
+      0
+    elsif high?(hole_cards[0]) and high?(hole_cards[1])
+      10000
     else
       0
     end
